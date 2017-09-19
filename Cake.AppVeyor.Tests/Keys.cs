3661c7e4f4d328329235804a733c1be57d06434a
Jonathan Dick <jondick@gmail.com>
2017-09-19 10:40:06 -0400

    Fix up tests and use dynamic api token

--- Cake.AppVeyor.Tests/Keys.cs
+++ Cake.AppVeyor.Tests/Keys.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+using System.IO;
 
 namespace Cake.AppVeyor.Tests
 {
     public static class Keys
     {
-        public static string AppVeyorApiToken = "{APPVEYOR_APITOKEN}";
+        const string YOUR_APPVEYOR_API_TOKEN = "{APPVEYOR_APITOKEN}";
+
+        static string appVeyorApiToken;
+
+        public static string AppVeyorApiToken {
+            get
+            {
+                if (appVeyorApiToken == null)
+                {
+                    // Check for a local file with a token first
+                    var localFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", ".appveyorapitoken");
+                    if (File.Exists(localFile))
+                        appVeyorApiToken = File.ReadAllText(localFile);
+
+                    // Next check for an environment variable
+                    if (string.IsNullOrEmpty(appVeyorApiToken))
+                        appVeyorApiToken = Environment.GetEnvironmentVariable("appveyor_api_token");
+
+                    // Finally use the const value
+                    if (string.IsNullOrEmpty(appVeyorApiToken))
+                        appVeyorApiToken = YOUR_APPVEYOR_API_TOKEN;
+                }
+
+                return appVeyorApiToken;
+            }
+        }
     }
 }
 
