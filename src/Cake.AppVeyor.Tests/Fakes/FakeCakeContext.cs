87042402efe744a9214abd48a76c19c97e9b0536
Nicolai Brogaard <nmb@minserver.dk>
2018-06-23 22:38:43 +0200

    Update to cake 0.28

--- src/Cake.AppVeyor.Tests/Fakes/FakeCakeContext.cs
+++ src/Cake.AppVeyor.Tests/Fakes/FakeCakeContext.cs
@@ -7,11 +7,49 @@ using Cake.Testing;
 
 namespace Cake.AppVeyor.Fakes
 {
+internal sealed class CakeDataService : ICakeDataService
+    {
+        private readonly Dictionary<Type, object> _data;
+
+        public CakeDataService()
+        {
+            _data = new Dictionary<Type, object>();
+        }
+
+        public TData Get<TData>()
+            where TData : class
+        {
+            if (_data.TryGetValue(typeof(TData), out var data))
+            {
+                if (data is TData typedData)
+                {
+                    return typedData;
+                }
+                var message = $"Context data exists but is of the wrong type ({data.GetType().FullName}).";
+                throw new InvalidOperationException(message);
+            }
+            throw new InvalidOperationException("The context data has not been setup.");
+        }
+
+        public void Add<TData>(TData value)
+            where TData : class
+        {
+            if (_data.ContainsKey(typeof(TData)))
+            {
+                var message = $"Context data of type '{typeof(TData).FullName}' has already been registered.";
+                throw new InvalidOperationException(message);
+            }
+            _data.Add(typeof(TData), value);
+        }
+    }
+
 	public class FakeCakeContext
 	{
 		ICakeContext context;
 		FakeCakeLog log;
 		DirectoryPath testsDir;
+
+	
 
 		public FakeCakeContext()
 		{
@@ -28,7 +66,8 @@ namespace Cake.AppVeyor.Fakes
 			var config = new Core.Configuration.CakeConfigurationProvider(fileSystem, environment).CreateConfiguration(testsDir, new Dictionary<string, string>());
 			var toolResolutionStrategy = new ToolResolutionStrategy(fileSystem, environment, globber, config);
 			var toolLocator = new ToolLocator(environment, toolRepo, toolResolutionStrategy);
-			context = new CakeContext(fileSystem, environment, globber, log, args, processRunner, registry, toolLocator);
+			var dataService = new CakeDataService();
+			context = new CakeContext(fileSystem, environment, globber, log, args, processRunner, registry, toolLocator, dataService);
 			context.Environment.WorkingDirectory = testsDir;
 		}
 
